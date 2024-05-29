a8ae9ad16704f6d6ea14674efd092edbbe9d519a
aghenage <aghenage@gmail.com>
2024-05-29 09:40:21 -0400

    add log.info

--- src/test/java/me/steps/StepDefs.java
+++ src/test/java/me/steps/StepDefs.java
@@ -37,6 +37,7 @@ public class StepDefs extends BasePage {
         String actualTitle = Driver.getDriver().getTitle();
         assertEquals(actualTitle,expectedTitle);
         log.info(actualTitle + expectedTitle);
+        log.info(expectedTitle);
 
     }
 
